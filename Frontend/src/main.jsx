9d57f3a6452281718d3358c23bb2af2b472140ba
ISHA <itank861@rku.ac.in>
2025-01-29 11:02:35 +0530

    Appointment Booking Page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import RegisterForm from './register.jsx'
+import BookingForm from './appointment.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RegisterForm/>
+    <BookingForm/>
   </StrictMode>,
 )
