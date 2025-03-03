b76a65a8baf69593a8ab1c2759f50b8b5f360a8c
DRASHTI CHAG <146415493+chagdrashti@users.noreply.github.com>
2025-03-03 08:32:04 +0530

    button call design

--- mybtn.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-// properties and state
-class MyButton extends React.Component {
-  render() {
-    const { xyz, abc } = this.props;
-    return <button disabled={xyz}>{abc}</button>;
-  }
-}
-export default MyButton;
