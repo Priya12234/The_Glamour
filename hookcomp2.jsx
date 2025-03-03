c5e4c13d5c5a024a78e982633b91ba9f683cb06a
DRASHTI CHAG <146415493+chagdrashti@users.noreply.github.com>
2025-03-03 08:29:02 +0530

    requirment change

---
+++ hookcomp2.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function StateExample() {
+    const [name, setName] = useState("rku");
+
+    return (
+        <div>
+            <h1>{name}</h1>
+            <input 
+                type="text" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+            />
+        </div>
+    );
+}
+
+export default StateExample;
